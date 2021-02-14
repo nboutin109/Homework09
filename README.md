af30a2553d641b564c72b3002ec6eb89899fc418
Nick Boutin <nboutin109@gmail.com>
2021-02-14 17:06:30 -0500

    polishing_still_ip

---
+++ README.md
@@ -0,0 +1,10 @@
+# Title
+README generator
+
+## Purpose
+A CLI application which can generate readme files
+
+## Usage
+Dependencies are the axios, fs, and inquirer NPM packages.  
+To use install the above packages and run with command node index.js
+undefinedundefined
--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-const questions = [
 
-];
+var inquirer = require("inquirer");
+var fs = require('fs');
+var axios = require("axios");
 
-function writeToFile(fileName, data) {
-}
+//inquirer prompts
 
-function init() {
+inquirer.prompt([
+  {
+    type: "input",
+    name: "username",
+    message: "What is your github username?"
+  },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Describe the purpose of your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How is it used?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What tests were used?",
+    name: "test",
+  }
+]).then(function(data) {
 
-}
+  //ajax call
+  var queryUrl = `https://api.github.com/users/${data.username}`
+  axios.get(queryUrl).then(function(response) {
+    var thestuff = response;
+    console.log(thestuff);
+    const userimg = thestuff.avatar_url;
+    const useremail = thestuff.email 
+    
 
-init();
+// writing file
+
+  fs.writeFile('README2.md', JSON.stringify(data, null, '\t'), function(err) {
+
+    if (err) {
+      return console.log(err);
+    }
+
+    fs.appendFile('README.md', userimg, function (err) {
+      if (err) throw err;
+    });
+
+    fs.appendFile('README.md', useremail, function (err) {
+      if (err) throw err;
+    });
+
+    console.log("File completed!");
+  });
+  });
+});
